0e3bc70ec465e9c467e004b77f5ff1ca25691165
akumaLin <172754827@qq.com>
2017-03-02 01:51:18 +0800

    首页基本，tab1

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,9 @@ export default {
 *{
   padding:0;
   margin:0;
+  -webkit-tap-highlight-color:rgba(255,0,0,0);
+  -webkit-font-smoothing : antialiased;
+
 }
 html{
   font-size: 1rem;
@@ -23,9 +26,11 @@ html,body{
   width:100vw;
   height:100vh;
 }
-a{
+a:hover, a:visited, a:link, a:active ,a,a:focus{
   text-decoration: none;
   color: #5a5a5a;
+  out-line: none;
+  -webkit-tap-highlight-color:rgba(255,0,0,0);
 }
 @import 'font/iconfont.css';
 </style>
